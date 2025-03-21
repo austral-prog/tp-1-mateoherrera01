8e9d804bf60d29e8c0f3927d74cca3cd795accee
mateoherrera01 <maherrera@mail.austral.edu.ar>
2025-03-21 10:40:51 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,12 @@
 def math():
     a = 57
     b = 7
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    promedio = (a + b) / 2
+    print(promedio)
+    print(a // b)
+    print(a % b)
+    print(a / b)
